01c589a0b5aac6dfd87151eebcb90b8eb09f30e7
MaHi <mahesh30795@gmail.com>
2018-12-05 16:27:20 +0530

    Change in testAPI.sh

--- testAPIs.sh
+++ testAPIs.sh
@@ -302,17 +302,16 @@ TRX_ID=$(curl -s -X POST \
  echo "Transaction ID is $TRX_ID"
 
 #Invoke chaincode on peers of all Orgs
-Medication={"MedName":"XXxx", "Compound":"ddddd", "Dosage":"ssdsdsd", "Quantity":"sss"}
 echo "POST invoke chaincode on peers of Org1 and Org2 and Org3"
 echo
 TRX_ID=$(curl -s -X POST \
   http://localhost:4000/channels/rxmed/chaincodes/mycc \
-  -H "authorization: Bearer $ORG1_TOKEN" \
+  -H "authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDQwMjgzNDcsInVzZXJuYW1lIjoiSmltIiwib3JnTmFtZSI6Ik9yZzEiLCJpYXQiOjE1NDM5OTIzNDd9.2WlftlCnQ0B4urLLul_zo6NJXA89c-m7SdzG0WwKjuw" \
   -H "content-type: application/json" \
   -d '{
 	"peers": ["peer0.org1.rxmed.com","peer0.org2.rxmed.com","peer0.org3.rxmed.com"],
 	"fcn":"createPatient",
-	"args":["PRESC1","PAT1","'$Medication'", "68101"]
+	"args":["PRESC1","PAT1","xXx","comp","dos","qty", "68101"]
 }')
  echo "Transaction ID is $TRX_ID"
 
